53d1d72aa87a653c6d007bee1b7ad9df41af7536
Emre Tufan <emretufan42@gmail.com>
2023-11-09 13:44:29 +0300

    feat(server): todo entity created

---
+++ server/src/main/java/com/emretufan/TodoAppBackend/entity/Todo.java
@@ -0,0 +1,22 @@
+package com.emretufan.TodoAppBackend.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+
+@Entity
+@Table(name = "todo")
+@Data
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "done")
+    private String done;
+
+}
